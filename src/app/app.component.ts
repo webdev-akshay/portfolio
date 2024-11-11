22db6788a8a70f0b32abe670aa3292d182ff338d
akshay-webdev <webdev.akshay@gmail.com>
2024-11-12 01:42:53 +0530

    ui changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ServicesComponent } from './components/services/services.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ToolsComponent } from './components/tools/tools.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NavbarComponent,HomeComponent,AboutComponent,ExperienceComponent,ServicesComponent,SkillsComponent,ToolsComponent,ProjectsComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ServicesComponent } from './components/services/services.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ToolsComponent } from './components/tools/tools.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'experience',component:ExperienceComponent},
+  {path:'services', component:ServicesComponent},
+  {path:'skills',component:SkillsComponent},
+  {path:'tools', component:ToolsComponent},
+  {path:'projects', component:ProjectsComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'',redirectTo:'/home', pathMatch:'full'},
+  {path:'**',component:PagenotfoundComponent}
+];
