b339ce42473430f58b2e15f087cb578c44038117
akshay-proctur <akshay@proctur.com>
2024-11-12 00:37:34 +0530

    proj created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ResumeComponent } from './components/resume/resume.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'/experience',component:ResumeComponent},
+
+];
