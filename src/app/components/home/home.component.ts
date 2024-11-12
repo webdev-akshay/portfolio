d062b38cb780427c5c2be13f4da2cdccc8e6069d
akshay-webdev <webdev.akshay@gmail.com>
2024-11-12 18:28:55 +0530

    ui changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
 import { ExperienceComponent } from '../experience/experience.component';
 import { RouterOutlet } from '@angular/router';
+import { ServicesComponent } from "../services/services.component";
+import { SkillsComponent } from "../skills/skills.component";
+import { ToolsComponent } from "../tools/tools.component";
+import { ProjectsComponent } from "../projects/projects.component";
+import { ContactComponent } from "../contact/contact.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet,AboutComponent,ExperienceComponent],
+  imports: [RouterOutlet, AboutComponent, ExperienceComponent, ServicesComponent, SkillsComponent, ToolsComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
